821af383c0306689500c6766a2efe3fb6faea830
Joshua Friend <joshua@codeethic>
2022-03-29 14:16:48 -0400

    - Add .gitignore - Add webapi artifacts

---
+++ webapi/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace todo_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    try
+                    {
+                        builder.AddSystemsManager((source) =>
+                        {
+                            source.Path = "/Database";
+                        });
+                    }
+                    catch { }
+                })
+            .UseStartup<Startup>()
+            .UseUrls("http://*:8080");
+    }
+}
